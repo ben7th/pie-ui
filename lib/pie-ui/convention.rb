8b85db34e77281f4b3e922fe17e8d2f244b386f1
ben7th <ben7th@sina.com>
2010-08-10 08:51:01 +0800

    updating mplist

--- lib/pie-ui/convention.rb
+++ lib/pie-ui/convention.rb
@@ -48,6 +48,17 @@ module PieUi
           ul_for_part
       end
     end
+
+    # 产生随机字符串
+    def randstr(length=8)
+      base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      size = base.size
+      re = ''<<base[rand(size-10)]
+      (length-1).times  do
+        re<<base[rand(size)]
+      end
+      re
+    end
     
   end
   
