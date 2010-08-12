e7a639c6b025d3ec6e3ff73d4cfaf1f07f82aa50
ben7th <ben7th@sina.com>
2010-08-12 18:07:46 +0800

    adding js css bundle helpers

--- lib/pie-ui.rb
+++ lib/pie-ui.rb
@@ -2,14 +2,25 @@ module PieUi
   class << self
     def enable_actionpack
       require 'pie-ui/convention'
+
       require 'pie-ui/mplist_helper'
-      require 'pie-ui/xml_format_helper'
       ActionView::Base.send :include, MplistHelper
+
+      require 'pie-ui/xml_format_helper'
       ActionView::Base.send :include, XmlFormatHelper
+
+      require 'pie-ui/bundle_helper'
+      ActionView::Base.send :include, BundleHelper
     end
   end
 end
 
 if defined? Rails
   PieUi.enable_actionpack if defined? ActionController
+  def Rails.production?
+    Rails.env == 'production'
+  end
+  def Rails.development?
+    Rails.env == 'development'
+  end
 end
