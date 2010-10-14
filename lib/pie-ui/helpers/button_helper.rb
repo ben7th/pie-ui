37c3c334191cec818140f58c2eca17ce36bc8b69
ben7th <ben7th@sina.com>
2010-10-14 15:12:42 +0800

    button

--- lib/pie-ui/helpers/button_helper.rb
+++ lib/pie-ui/helpers/button_helper.rb
@@ -2,4 +2,8 @@ module ButtonHelper
   def minibutton_remote(name, options = {}, html_options = {}, &block)
     link_to_remote("<span>#{name}</span>",options,html_options.merge({:class=>'minibutton'}),&block)
   end
+
+  def minibutton(name, url, options = {}, &block)
+    link_to("<span>#{name}</span>",url,options.merge({:class=>'minibutton'}),&block)
+  end
 end
